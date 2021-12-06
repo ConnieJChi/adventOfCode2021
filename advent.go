88e722c21e29cd30933a2d363b3448c73c3f4b7f
ConnieJChi <conniejchi@berkeley.edu>
2021-12-05 23:20:49 -0800

    Problem 4 Part 1 & 2 done

--- advent.go
+++ advent.go
@@ -125,6 +125,83 @@ func problem3() {
     fmt.Println(co2 * oxygen)
 }
 
+func checkForBingo(bingoSheet [][] string, indexRow int, indexCol int) [][] string {
+    // check the horizontal/vertical bingo for current sheet
+    boolHorz := true
+    boolVert := true
+    for i := 0; i < 5; i+= 1 {
+        boolHorz = boolHorz && (bingoSheet[indexRow][i] == "-1")
+        boolVert = boolVert && (bingoSheet[i][indexCol] == "-1")
+    }
+    if boolHorz || boolVert {
+        return bingoSheet
+    } else {
+        return nil
+    }
+}
+
+func problem4() {
+    file, err := os.Open("input4.txt")
+    check(err)
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    var inputArr []string
+
+    for scanner.Scan() {
+        inputArr = append(inputArr, scanner.Text())
+    }
+
+    bingoInput := strings.Split(inputArr[0], ",")
+
+    var bingoCharts [][][] string
+
+    for i := 2; i < len(inputArr); i += 6 {
+        var appendArr [][]string
+        for j := i; j < i + 5 && j < len(inputArr); j += 1 {
+            appendArr = append(appendArr, strings.Split(strings.TrimSpace(inputArr[j]), " "))
+        }
+        bingoCharts = append(bingoCharts, appendArr)
+    }
+
+    inputIndex := 0
+    for inputIndex < len(inputArr) {
+        for indexSheet, bingoSheet := range bingoCharts {
+            if bingoSheet[0][0] == "W" {
+                continue
+            }
+            for indexRow, bingoRow := range bingoSheet {
+                for indexCol, value := range bingoRow {
+                    if bingoInput[inputIndex] == value {
+                        prevVal := value
+                        bingoCharts[indexSheet][indexRow][indexCol] = "-1"
+                        if checkForBingo(bingoSheet, indexRow, indexCol) != nil {
+                            sum := 0
+                            for _, thingRow := range bingoSheet {
+                                for _, thingVal := range thingRow {
+                                    if thingVal != "-1" {
+                                        temp, err := strconv.Atoi(thingVal)
+                                        check(err)
+                                        sum += temp
+                                    }
+                                }
+                            }
+                            temp, err := strconv.Atoi(prevVal)
+                            check(err)
+                            fmt.Println(temp, sum)
+                            bingoSheet[0][0] = "W"
+                        }
+                    }
+
+                }
+            }
+        }
+        inputIndex += 1
+    }
+    // fmt.Println(bingoCharts)
+}
+
 func main() {
-    problem3()
+    problem4()
 }
